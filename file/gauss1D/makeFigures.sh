f68406efeade13213f7bde22c571ec440918e355
Frans van der Meer <f.p.vandermeer@tudelft.nl>
2025-12-17 15:27:52 +0100

    Add gauss1D

---
+++ file/gauss1D/makeFigures.sh
@@ -0,0 +1,9 @@
+fig2dev -L eps -D +49,50,52,53 gauss1D.fig gauss1D1.eps
+fig2dev -L eps -D +49,50,55,56,57 gauss1D.fig gauss1D3.eps
+fig2dev -L eps -D +49,50,58,59,60 gauss1D.fig gauss1D5.eps
+
+latex gauss1D.tex
+dvips gauss1D.dvi
+ps2pdf gauss1D.ps
+
+pdftoppm gauss1D.pdf gauss1D -r 600 -png -singlefile
